59d294b7b77585dd313a42aa9c7aedc8519c100e
ksm9606 <54825978+ksm9606@users.noreply.github.com>
2021-01-26 23:35:42 +0900

    ch2 라이트박스

---
+++ ch2/lightBox.js
@@ -0,0 +1,28 @@
+var indicator = document.querySelectorAll('.indicator button')
+var lightbox = document.querySelector("#lightbox");
+var block = document.querySelector('#block');
+
+// 라이 박스 보기
+function lightboxOpen(num){
+    lightbox.setAttribute('class', 'active');
+    block.setAttribute('class', 'active'); 
+
+    changeImg(num);
+    indicator[num-1].focus();
+}
+
+// 라이 박스 닫기
+function lightboxClose(){
+    lightbox.removeAttribute('class');
+    block.removeAttribute('class');
+}
+
+// 이미지 전환 표시
+function changeImg(num){
+    var imgs = document.querySelectorAll('figure > img');
+
+    for(var i=0; i<imgs.length; i++){
+        imgs[i].removeAttribute('class');
+    }
+    imgs[num-1].setAttribute('class', 'active');
+}
