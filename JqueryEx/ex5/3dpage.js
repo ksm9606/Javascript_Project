0894f148d308664dda822f0d65ad36d4b7d7afcc
ksm9606 <54825978+ksm9606@users.noreply.github.com>
2021-02-17 18:24:57 +0900

    Jquery_3dpage

---
+++ JqueryEx/ex5/3dpage.js
@@ -0,0 +1,60 @@
+var $page = $('.page'),
+    indicator_li = document.querySelectorAll('li');
+
+var yDeg = 0,
+    indicator_num = 1,
+    indicator_length = $page.length,
+    w = $page[0].offsetWidth,
+    page_angle = 0;
+
+
+// 페이지 초기화
+function init_page(){
+    w = $page[0].offsetWidth;
+
+    // 3D page 4면체 위치 정의
+    for(var i=0; i<$page.length; i++){
+        $page[i].style.transform = 'rotateY(' + page_angle + 'deg) translateZ(' + (w/2) + 'px)' ;
+        page_angle += 90;
+    }
+
+    // page wrapper 정면으로 초기화
+    $('.wrapper').css('transform', 'translateZ(' + (-w/2) + 'px) rotateY(' + yDeg + 'deg)');
+}
+
+
+// 인디케이터 초기화
+function init_indicator(){
+    for(var i=0; i<indicator_length; i++){
+        $('#indicator').append('<li>' + (i+1) + '</li>');
+    }
+    indicator_li = indicator.querySelectorAll('li');
+    change_page(indicator_num); 
+}
+
+function change_page(indicator_num){
+    indicator_li = indicator.querySelectorAll('li');
+    // 현재 인디케이터 하이라이트 표시
+    indicator_li[indicator_num - 1].setAttribute('class', 'active');
+    
+    yDeg = -90 * (indicator_num-1)
+    $('.wrapper').css('transform', 'translateZ(' + (-w/2) + 'px) rotateY(' + yDeg + 'deg)');
+
+    // 인디케이터 표시
+    for(var i=0; i<indicator_li.length; i++){
+        indicator_li[i].removeAttribute('class');
+    }
+    indicator_li[indicator_num - 1].setAttribute('class', 'active');
+}
+
+init_page();
+init_indicator();
+
+
+// 이벤트 리스너
+for(var i=0; i<indicator_li.length; i++){
+    indicator_li[i].addEventListener('click', function(){
+        indicator_num = parseInt(this.innerText);
+        change_page(indicator_num);
+    })
+}
